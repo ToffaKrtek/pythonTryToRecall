a0a11a9473c9eeb7d5ce98a1499f45f256e40db3
ToffaKrtek <krotov.anthon@yandex.ru>
2022-07-19 15:49:15 +0700

    наброски моделей

--- app/app/urls.py
+++ app/app/urls.py
@@ -15,7 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.urls import include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('index/', include('index.urls')),
+]
+from django.views.generic import RedirectView
+urlpatterns += [
+    path('', RedirectView.as_view(url='/index/', permanent=True)),
 ]
---
+++ app/index/models.py
@@ -0,0 +1,25 @@
+from django.db import models
+
+# Create your models here.
+class Message(models.Model):
+    text = models.CharField(max_length=240)
+    user_name = models.CharField(max_length=100)
+    user_lastname = models.CharField(max_length=100)
+    date = models.DateTimeField(auto_now_add=True, blank=True)
+    def __str__(self):
+        return self.user_name + ' ' + self.user_lastname + '\n' + self.text + '\n' + self.date
+
+
+class Contact(models.Model):
+    title = models.CharField(max_length=240)
+    text = models.CharField(max_length=240)
+    def __str__(self):
+        return self.title + '' + self.text
+
+class BotCommand(models.Model):
+    command = models.CharField(max_length=20)
+    description = models.CharField(max_length=240)
+    status = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.command + ' -- ' + self.description
